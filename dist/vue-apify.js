0c5d7d46c75b5b8a0b6171cca1427ea6f33ac6dd
Rafael <fahreevrafael@yandex.ru>
2017-04-09 00:27:22 +0300

    test publish

--- dist/vue-apify.js
+++ dist/vue-apify.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global['vue-apify'] = factory());
+  (global.VueApify = factory());
 }(this, (function () { 'use strict';
 
 var __cov_RlZ9axv4MRpE$16_ASuQNQ = (Function('return this'))();
