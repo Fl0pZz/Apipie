e58c6ebb809bd9b80498df0d35767235b50994ac
Nikita Nafranets <eddimensi@gmail.com>
2017-09-24 00:54:04 +0300

    done, write test and release feature, settings babel

--- tests/args-parser.test.js
+++ tests/args-parser.test.js
@@ -6,6 +6,7 @@ describe('parseExecArgs', () => {
   test('basic', () => {
     expect(parseExecArgs(url, { params: { id: 1 } }, { _require: { query: false, data: false } })).toEqual({ url: '/test/1' })
   })
+
   test('all', () => {
     const payload = {
       params: { id: 1 },
@@ -19,5 +20,65 @@ describe('parseExecArgs', () => {
       data: { test: 'test' }
     }
     expect(result).toEqual(expected)
+  })
+})
+
+describe('parseExecArgs - testing urlToParse', () => {
+  const url = '/api/v1/foo/:id/bar/:test'
+  const requiredProps = { _require: { query: false, data: false } }
+
+  test('test - 1, put all args', () => {
+    const props = {
+      urlToParse: 'http://test-site.com/api/v1/foo/1/bar/test?page=1&limit=10'
+    }
+
+    const expectResult = {
+      url: '/api/v1/foo/1/bar/test',
+      params: {
+        page: '1',
+        limit: '10'
+      }
+    }
+
+    const result = parseExecArgs(url, props, requiredProps)
+    expect(result).toEqual(expectResult)
+  })
+
+  test('test - 2, put without query', () => {
+    const props = {
+      urlToParse: 'http://test-site.com/api/v1/foo/1/bar/foobar'
+    }
+
+    const expectResult = {
+      url: '/api/v1/foo/1/bar/foobar',
+      params: {}
+    }
+
+    const result = parseExecArgs(url, props, requiredProps)
+    expect(result).toEqual(expectResult)
+  })
+
+  test('test - 3, put not enogth params', () => {
+    const props = {
+      urlToParse: 'http://test-site.com/api/v1/foo/1/bar'
+    }
+
+    const result = () => {
+      parseExecArgs(url, props, requiredProps)
+    }
+
+    expect(result).toThrowError(/Mismatch the path or not enough params for this url: /)
+  })
+
+  test('test - 4, mismatch path', () => {
+    const props = {
+      urlToParse: 'http://test-site.com/api/v1/users/'
+    }
+
+    const result = () => {
+      parseExecArgs(url, props, requiredProps)
+    }
+
+    expect(result).toThrowError(/Mismatch the path or not enough params for this url: /)
   })
 })
